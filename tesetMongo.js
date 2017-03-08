cd59ca857eccaf6990778e1f9e11f17dcff8f662
daiyayun <dyy06148@163.com>
2017-03-08 21:00:22 +0100

    hehe

--- tesetMongo.js
+++ tesetMongo.js
@@ -5,7 +5,7 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://192.168.3.6:27017/grifs';
+var url = 'mongodb://localhost:27017/grifs';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
