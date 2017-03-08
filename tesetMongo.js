2a3d273e7ff7ce4a99a16416da308450b7ba7454
lazerjet <zejian.li14@sps.nus.edu.sg>
2017-03-08 19:35:21 +0100

    find

--- tesetMongo.js
+++ tesetMongo.js
@@ -5,7 +5,8 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://192.168.3.6:27017/grifs';
+var url1 = 'mongodb://192.168.3.6:27017/grifs';
+var url='mongodb://localhost:27017/test';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
@@ -17,10 +18,11 @@ MongoClient.connect(url, function (err, db) {
 
     // do some work here with the database.
     var collection=db.collection('fs.files');
-    collection.find().toArray(function(err,docs){
+    collection.find({filename:'test_do.docx'}).toArray(function(err,docs){
     	if(err){console.log('error reading collection')}
     		else{
     			console.log(docs);
+    			console.log(docs[0].md5);
     		}
     })
 
